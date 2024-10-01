67714da946c4a6ef1e252a784380d920421f3d04
g-ivanova <geryy.ivanovaa@gmail.com>
2024-10-01 09:32:46 +0300

    Fixed functionality for editing user from the admin menu. Redirect all edit group and edit user buttons except cancel to refresh the same page.

---
+++ target/classes/static/js/sendpass.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+   $("#role").change(function() {
+   var rolevalue=$('#role').val();
+    $('#user').find('option').remove();
+         $('#user').append($('<option>', {
+                       value: 0,
+                       text: 'Select User:'
+                   }));
+       var roleId = $('#role').val();
+            var inputValObj = {};
+           // alert(distid1);
+            inputValObj.Discode = roleId;
+            var inputVal = JSON.stringify(inputValObj);
+          //  alert(inputVal);
+            var data = inputVal.toString();
+          //  alert(data);
+
+      	$.ajax({
+      	type: "GET",
+        url : "http://localhost:8080/getUsersFromRole?roleId="+roleId,
+        contentType: "application/json",
+        success: function (data) {
+            let obj = $.parseJSON(data);
+            $.each(obj, function (key, value) {
+            $('<option>').val(value.user_id).text(value.user_name).appendTo(user);
+             //  $('#city').append('<option value="'  + value.districtcode + '">' + value.districtcode + '--' + value.districtname + '</option>');
+            });
+         },
+         error: function (data) {
+            $('#city').append('<option>User not available</option>');
+         },
+      });
+
+   });
+   });
