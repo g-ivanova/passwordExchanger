b2b461330741e2977f592d84197bb95736c2c3b5
desislava-krasteva <desislava.kr.krusteva@gmail.com>
2024-10-17 11:26:53 +0300

    PASSEX-22 Created confirmation pop-ups

---
+++ target/classes/static/js/confirmationPopUp.js
@@ -0,0 +1,14 @@
+let alertBox = document.getElementById("customAlertBox");
+let custom_button = document.querySelector(".custom-button");
+let close_img = document.querySelector(".close");
+let body = document.querySelector("body");
+let close_btn = document.getElementById("cancel-alert");
+custom_button.addEventListener('click', function () {
+    alertBox.style.display = "block";
+});
+close_img.addEventListener('click', function () {
+    alertBox.style.display = "none";
+});
+close_btn.addEventListener('click', function () {
+    alertBox.style.display = "none";
+});
