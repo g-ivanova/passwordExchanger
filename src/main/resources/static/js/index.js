94fab7a5f09e9dff0482210cb47689c7f49708fa
desislava-krasteva <desislava.kr.krusteva@gmail.com>
2024-11-07 16:20:23 +0200

    PASSEX-4 Changed sign-in button to be enabled only on filled fields

---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    function toggleSubmitButton() {
+        // Check if the form is valid by testing all the fields
+        if ($('#username').val() !== "" &&
+           $('#password').val() !== "" ) {
+            $('#SignIn').prop('disabled', false); // Enable the button
+        } else {
+            $('#SignIn').prop('disabled', true); // Disable the button
+       }
+    }
+    // Focusout event listeners for inputs
+    $('#username').focusout(function () {
+        toggleSubmitButton();
+    });
+
+    $('#password').focusout(function () {
+        toggleSubmitButton();
+    });
+
+    // Initial button state check
+    toggleSubmitButton();
+});
